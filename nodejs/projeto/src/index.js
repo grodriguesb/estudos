5915ac9caf4079904b6497c517109bd02e2b8460
Gabriel Barros <gabrielr.barros@outlook.com>
2026-01-27 20:59:53 -0300

    Parametros e AWAIT

--- nodejs/projeto/src/index.js
+++ nodejs/projeto/src/index.js
@@ -50,7 +50,13 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+async function playRaceEngine(character1, character2) {
+
+}
+
 (async function main() {
     console.log(`🏁🚦Corrida entre ${player1.NOME} e ${player5.NOME} começando...\n`)
+
+    await playRaceEngine(player1, player2)
 })()
 
