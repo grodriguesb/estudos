c4227b604087148028a0d8d377bdd9280f68115d
Gabriel Barros <gabrielr.barros@outlook.com>
2026-01-28 19:22:03 -0300

    Switch Cases
    Realizado a lógica do switch case para retornar um resultado assim que uma das opções forem verdadeira

--- nodejs/projeto/src/index.js
+++ nodejs/projeto/src/index.js
@@ -50,11 +50,32 @@ async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+async function getRandomBlock() {
+    let random = Math.random()
+    let result 
+
+    switch (true) {
+        case ramdon < 0.33:
+            result = "RETA"
+            break;
+        
+        case random < 0.66:
+            result = "CURVA"
+            break;
+        
+        default:
+            result = "CONFRONTO"
+    }
+    return result
+}
+
 async function playRaceEngine(character1, character2) {
     for (let round = 1; round <= 5; round++) {
         console.log(`🏁 Rodada ${round}`)
         
         // sortear bloco
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`)
     }
 }
 
