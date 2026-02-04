8c958bae8782e26a084b34e65e43bde24ebdd3ec
Gabriel Barros <gabrielr.barros@outlook.com>
2026-02-04 19:42:11 -0300

    Export Default

--- nodejs/04-commonjs-modules/src/main.js
+++ nodejs/04-commonjs-modules/src/main.js
@@ -1,13 +1,15 @@
 const product = require("./services/products")
 const config = require("./services/config")
+const database = require("./services/database")
 
 async function main() {
     console.log('Carrinho de compras:')
-    // p.getFullName("408", "mousepad")
-    // p.getFullName("324", "mouse")
+    // product.getFullName("408", "mousepad")
+    // product.getFullName("324", "mouse")
 
     product.
     console.log(config.devArea)
+    database.conectToDatabase
 }
 
 main()
---
+++ nodejs/04-commonjs-modules/src/services/database.js
@@ -0,0 +1,24 @@
+// export default
+
+exports.conectToDatabase = (dataName) => {
+    console.log("se conectando ao banco: " + dataName)    
+}
+
+exports.disconnectDatabase = () => {
+    // lógica
+    console.log("desconectando")
+}
+
+// formato padrão
+/*
+async function conectToDatabase(dataName) {
+    console.log("se conectando ao banco: " + dataName)    
+}
+
+async function disconnectDatabase() {
+    // lógica
+    console.log("desconectando")
+}
+
+*/
+
