57064b2abb1924f0333a468cab96822e163ecfbb
Gabriel Barros <gabrielr.barros@outlook.com>
2026-02-04 19:52:25 -0300

    Destructuring

--- nodejs/04-commonjs-modules/src/main.js
+++ nodejs/04-commonjs-modules/src/main.js
@@ -1,13 +1,15 @@
-const product = require("./services/products")
+// importação de modulos
+const {getFullName, productType} = require("./services/products")
 const config = require("./services/config")
 const database = require("./services/database")
 
 async function main() {
     console.log('Carrinho de compras:')
-    // product.getFullName("408", "mousepad")
-    // product.getFullName("324", "mouse")
+    //product.getFullName("408", "mousepad")
+    //product.getFullName("324", "mouse")
 
-    product.
+    getFullName("1", "teclado")
+
     console.log(config.devArea)
     database.conectToDatabase
 }
