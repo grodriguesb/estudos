3ed6c36b45294db5b002363f7502061edefd2814
Gabriel Barros <gabrielr.barros@outlook.com>
2026-02-09 19:54:29 -0300

    Destructuring

--- nodejs/05-esm-modules/src/index.js
+++ nodejs/05-esm-modules/src/index.js
@@ -1,7 +1,12 @@
 // import connectToDatabase from './utils/database.js'
-import * as database from './utils/database.js'
-
-
-database.connectToDatabase("my-database")
 
 //connectToDatabase("my-database")
+
+//import * as database from './utils/database.js'
+
+//database.connectToDatabase("my-database")
+
+import {disconnectDatabase, connectToDatabase} from "./utils/database.js"
+
+disconnectDatabase()
+connectToDatabase()
