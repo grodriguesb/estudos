4f26b505320bef8ec3cb854d9ab0d8481a53b38a
Gabriel Barros <gabrielr.barros@outlook.com>
2026-02-10 19:12:44 -0300

    Estuturando Cart.js
    Realizado a estrutura de como irá funcionar e quais funções serão nescessárias para o funcionamento das ações do carrinho como adicionar, remover, diminuir itens e somar o total dos produtos

---
+++ nodejs/projetos/shopee-cart/src/services/cart.js
@@ -0,0 +1,23 @@
+// Quais ações meu carrinho pode fazer
+
+
+// CASOS DE USO
+//-> Adicionar item no carrinho
+async function addItem(userCart) {
+    
+}
+
+//-> Deletar item do carrinho
+async function deleteItem(userCart, name) {
+    
+}
+
+//-> Remover um item - diminui um item
+async function removeItem(userCart, index) {
+    
+}
+
+//-> Calcular o total
+async function calculateTotal(userCart) {
+    
+}
